3217476d3dbebc36f33af00e73e0ce04803855a4
Andrii Vlasiuk <Andrii_Vlasiuk@epam.com>
2018-11-12 18:28:43 +0200

    Main Java functional interfaces added

--- src/main/java/module1LambdasStreams/task1/JavaFunctionalInterfacesImplementation.java
+++ src/main/java/module1LambdasStreams/task1/JavaFunctionalInterfacesImplementation.java
@@ -1,12 +1,43 @@
 package module1LambdasStreams.task1;
 
-import java.util.function.Supplier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.*;
 
 public class JavaFunctionalInterfacesImplementation {
 
     public static void main(String[] args) {
-        Supplier<String> supplier = ()->"supplierValue";
 
-        System.out.println(supplier.get());
+        List<Integer> values = Arrays.asList(3, 5, 8, 9, 12);
+        List<String> incorrectNames = Arrays.asList("Geek","GeeksQuiz","g1","QA","Geek2");
+        List<String> correctNames = Arrays.asList("John","Mark","Hanna");
+
+        System.out.println("//------------");
+
+        Supplier<String> supplier = ()->"supplierValue";
+        System.out.println("Supplier: " + supplier.get());
+
+        System.out.println("//------------");
+
+        Predicate<String> predicate = (s)->s.startsWith("G");
+        incorrectNames.stream()
+                .filter(predicate)
+                .forEach(System.out::println);
+
+        System.out.println("//------------");
+
+        Consumer<String> consumer = (name) -> System.out.println("Hello " + name);
+        correctNames.forEach(consumer);
+
+        System.out.println("//------------");
+
+        UnaryOperator<String> toUpperCaesOperator = String::toUpperCase;
+        correctNames.replaceAll(toUpperCaesOperator);
+        correctNames.forEach(System.out::println);
+
+        BinaryOperator<Integer> sumOperator = (i1, i2) -> i1 + i2;
+        int sum = values.stream()
+                .reduce(0, sumOperator);
+        System.out.println("sum=" + sum);
     }
 }
